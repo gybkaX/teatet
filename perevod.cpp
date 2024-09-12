fbf0b9d53df86558d3e0505d7e3d47ee3f24f232
gybkaX <167538403+gybkaX@users.noreply.github.com>
2024-09-12 22:49:33 +0700

    Add files via upload

---
+++ perevod.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+string perevod(int b, int n) {
+    // n это число в 10 b система в которую нужно перевести
+
+    string result = "";
+
+    for (int i = 0; n > 0; i++) {
+        if (10 == b) {
+            result = result;
+            break;
+        }
+
+        else {
+            if (n % b > 9) {
+                result = char('A' + (n % b) - 10) + result;
+            }
+            else {
+                result = char((n % b) + '0') + result;
+            }
+        }
+
+        n /= b;
+    }
+
+    return result;
+}
+
+int desiti(string num, int a) {
+    char z;
+    int x;
+    int result = 0;
+    if (a == 10) {
+        for (int i = 0; i <= num.length(); i++) {
+            z = num[i];
+            if (z >= '0' && z <= '9' && (z - '0')) {
+                result = result +( z - '0') * (pow(a, num.length() - i - 1));
+            }
+        }
+        return result;
+    }
+    else {
+        for (int i = 0; i <= num.length(); i++) {
+            z = num[i];
+            int step = num.length() - i;
+            if (z >= '0' && z <= '9' && (z - '0')) {
+                result = result + (z - '0') * (pow(a, num.length() - i - 1));
+            }
+            if(z >= 'A' && z <= 'Z' && (z - 'A')) {
+                result = result + (z - 'A' + 10) * (pow(a, num.length() - i - 1));
+            }
+            if (z == 'A') {
+                result = result + 10 * (pow(a, num.length() - i - 1));
+            }
+        }
+        return result;
+    }
+}
+
+
+
+int main()
+{
+    string Num;
+    char Z;
+    int A, B;
+    cout << "Vvedi Sistem iz kakaoi\n";
+    cin >> A;
+    cout << "V kakiy?\n";
+    cin >> B;
+    cout << "Chislo?\t Vvodi bykvi tolko zaglavnimi\n";
+    cin >> Num;
+    int x = desiti(Num, A);
+    cout <<"desitichai\t" << x << "\n";
+    cout << "itog\t" << perevod(B, x);
+}
